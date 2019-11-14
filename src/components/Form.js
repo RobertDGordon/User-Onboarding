60270476b74bad9cfc862c37781669a0b5866aec
Robert Gordon <Komeuppance@gmail.com>
2019-11-14 12:53:39 -0800

    Form.js initial setup complete, added styled-components

---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import {withFormik, Form, Field} from "formik";
+import * as Yup from 'yup';
+import axios from 'axios';
+import styled from "styled-components";
+
+const UserForm = ()=> {
+
+
+    return (
+        <div></div>
+    )
+}
+
+const FormikUserForm = withFormik({
+
+})
+
+export default FormikUserForm;
