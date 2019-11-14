9be5e056868a7c8ad9759f622ad9260cbbdf42fa
Robert Gordon <Komeuppance@gmail.com>
2019-11-14 15:02:38 -0800

    Stretch goals complete

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ const MainApp = styled.div `
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  /* justify-content: center; */
   font-size: calc(10px + 2vmin);
   color: white;
+  margin: 0 auto;
 
 `
 
 function App() {
+
   return (
     <MainApp>
       <UserForm />
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,23 +3,28 @@ import {withFormik, Form, Field} from "formik";
 import * as Yup from 'yup';
 import axios from 'axios';
 import styled from "styled-components";
-import { createHash } from "crypto";
+// import { createHash } from "crypto";
 
 const FormInput = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    /* justify-content: center; */
-    width: 270px;
-    height: 420px;
+    justify-content: center;
+    text-align: center;
+    width: 250px;
+    height: 250px;
     border: 1px solid black;
     background-color: white;
     border-radius: 10px;
-    margin: 30px;
-    margin-bottom: 130px;
-    padding: 10px;
+    margin: 30px auto;
+    padding: 10px 20px;
     box-shadow: 2px 2px darkgray;
     color:black;
+    button{
+        margin-top: 30px;
+        width: 60%;
+        height: 40px;
+    }
 `
 
 const ErrorMsg = styled.div`
@@ -27,14 +32,65 @@ const ErrorMsg = styled.div`
     font-size: 0.8rem;
 `
 
+const Dropdown = styled.div`
+    display: flex;
+    justify-content: center;
+    /* align-items: center; */
+    margin: 10px 0px;
+    font-size: 0.9rem;
+    text-align: center;
+    .select{
+        margin: 0 10px;
+    }
+
+`
+
 const Checkbox = styled.div `
     display: flex;
     justify-content: center;
-    align-items: center;
+    /* align-items: center; */
     /* border: 1px solid red; */
+    margin-right: 7px;
     font-size: 0.8rem;
-    text-align: center;
+    /* text-align: center; */
+`
 
+const Cards = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 90%;
+    margin: 0 auto;
+`
+
+
+const UserCard = styled.div `
+    display: flex;
+    font-size: 1rem;
+    flex-direction: column;
+    /* align-items: center; */
+    justify-content: center;
+    /* text-align: center; */
+    /* width: 250px; */
+    /* height: 250px; */
+    border: 1px solid black;
+    background-color: white;
+    border-radius: 10px;
+    margin: 30px;
+    /* margin-bottom: 130px; */
+    padding: 10px 20px;
+    box-shadow: 2px 2px darkgray;
+    color:black;
+    li {
+        margin: 5px;
+    }
+
+    .heading{
+        font-weight: bold;
+    }
+    .email {
+        font-style: italic;
+    }
 `
 
 const UserForm = ({values, errors, touched, status})=> {
@@ -55,6 +111,14 @@ const UserForm = ({values, errors, touched, status})=> {
                     {touched.email && errors.email && (<ErrorMsg>{errors.email}</ErrorMsg>)}
                     <Field type='password' name='password' placeholder='Password' />
                     {touched.password && errors.password && (<ErrorMsg>{errors.password}</ErrorMsg>)}
+                    <Dropdown>
+                        Know your role:
+                        <Field as="select" name="role" className='select'>
+                        <option value="Jabroni">Jabroni</option>
+                        <option value="Dropbear">Drop Bear</option>
+                        <option value="Snipe">Snipe</option>
+                        </Field>
+                    </Dropdown>    
                     <Checkbox>
                         <label>
                         Make me an offer I can't refuse
@@ -64,30 +128,36 @@ const UserForm = ({values, errors, touched, status})=> {
                     </Checkbox>
                     <button type='submit'>Log In</button>
                 </Form>
-                {users.map(user => (
-                    <ul key={user.id}>
-                        <li>Name: {user.name}</li>
-                        <li>Email: {user.email}</li>
-                    </ul>
-                ))}
             </FormInput>
+            <Cards>
+            {users.map(user => (
+                <UserCard key={user.id}>
+                    <ul>
+                        <li><span className="heading">Name:</span> {user.name}</li>
+                        <li><span className="heading">Email:</span> <span className='email'>{user.email}</span></li>
+                        <li><span className="heading">Role:</span> {user.role}</li>
+                    </ul>
+                </UserCard>
+                ))}
+            </Cards>
         </>
     )
 }
 
 const FormikUserForm = withFormik({
-    mapPropsToValues({name, email, password, tos}){
+    mapPropsToValues({name, email, password, role, tos}){
         return {
             name: name || '',
             email: email || '',
             password: password || '',
+            role: role || 'Jabroni',
             tos: tos || false
         };
     },
     validationSchema: Yup.object().shape({
         name: Yup.string().required('User name required!'),
-        email: Yup.string().required('Not a valid email!'),
-        password: Yup.string().required('Incorrect password!'),
+        email: Yup.string('waffle@syrup.com', 'Email is already taken').email().required('Not a valid email!'),
+        password: Yup.string().min(3, 'Minimum 3 characters').required('Incorrect password!'),
         tos: Yup.bool().oneOf([true],('Trust me...'))
     }),
     handleSubmit(values, {setStatus}){
