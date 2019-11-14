e0a16909a286623a5654a6842c8b9c40adec57a3
Robert Gordon <Komeuppance@gmail.com>
2019-11-14 13:04:20 -0800

    Initial styles setup, form displaying

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form'
+import UserForm from './components/Form'
+import styled from 'styled-components'
+
+const MainApp = styled.div `
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Formik
-      </header>
-    </div>
+    <MainApp>
+      <UserForm />
+    </MainApp>
   );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,16 +4,46 @@ import * as Yup from 'yup';
 import axios from 'axios';
 import styled from "styled-components";
 
-const UserForm = ()=> {
+const FormInput = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 270px;
+    height: 420px;
+    border: 1px solid black;
+    background-color: white;
+    border-radius: 10px;
+    margin: 30px;
+    margin-bottom: 130px;
+    padding: 10px;
+    box-shadow: 2px 2px darkgray;
+`
 
+const UserForm = ({values, errors, touched, status})=> {
+
+    const [users, setUsers] = useState ([]);
 
     return (
-        <div></div>
+        <>
+            <FormInput>
+                <Form>
+                    <Field type='text' name='name' placeholder='User name' />
+                </Form>
+            </FormInput>
+        </>
     )
 }
 
 const FormikUserForm = withFormik({
+    mapPropsToValues({name, email, password, tos}){
+        return {
+            name: name || '',
+            email: email || '',
+            password: password || '',
+            tos: tos || false
+        };
+    }
 
-})
+})(UserForm);
 
 export default FormikUserForm;
